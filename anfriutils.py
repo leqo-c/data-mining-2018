67cf36134f38ea23485d07de78915867529b78d4
leqo-c <leonardocariaggi@gmail.com>
2018-10-22 18:52:59 +0200

    Final version

---
+++ anfriutils.py
@@ -0,0 +1,39 @@
+
+def SPMFin_to_list(filename):
+    import numpy as np
+    import re
+    
+    l = list()
+    with open(filename, 'r') as f:
+        for line in f:
+            s = re.sub('<[0-9]+> ', '', line)
+            s = re.sub(' -1 -2', '', s)
+            s = re.sub(' -1', ',', s)            
+            s = s.split(',')
+            s = [int(el) for el in s]
+            l.append(s)
+
+    return l
+            
+
+def SPMFout_to_list(filename, reverse=True):
+    import re
+    
+    l = list()
+    with open(filename, 'r') as f:
+        for line in f:
+            s = re.sub('<[0-9]+> ', '', line)
+            s = re.sub(' -1', ',', s)
+            s = re.sub(' #SUP: ', '', s)
+            s = s.split(',')
+            s = [int(el) for el in s]
+            l.append( (s[:-1], s[-1]) )
+        
+    return sorted(l, key=lambda (pattern, sup): sup + len(pattern), reverse=reverse)
+    
+    
+def occurrences(seq, pattern):
+
+	seq_list = [val for val in seq]
+	occs = [(i, i+len(pattern)) for i in range( len(seq_list) ) if seq_list[i:i+len(pattern)] == pattern]
+	return occs 
---
+++ outliers/outliers.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun  3 22:25:12 2018
+
+@author: leo
+"""
+
+import numpy as np
+from sklearn.cluster import DBSCAN
+
+def dbscan_search(df, min_pts):
+
+    xx = np.linspace(0.01, 1.00, 200)
+    yy = list()
+    for eps in xx:
+        dbscan = DBSCAN(eps, min_samples=min_pts, metric='euclidean')
+        dbscan.fit(df)
+        labels = dbscan.labels_
+        # yy = size of noise cluster
+        yy.append(float(len([1 for i in labels if i == -1])) / len(df.values))
+    
+    my_eps = min( [ xx[i] for i in range(len(xx)) if yy[i]<=0.01 ] )
+    
+    return xx,yy,my_eps
